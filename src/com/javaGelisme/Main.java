c6341af3a477d96c3cfcfff95585d07256172abb
latifkara <latifkara93@gmail.com>
2022-12-19 18:30:12 +0300

    Main1 | Updata the variable

--- src/com/javaGelisme/Main.java
+++ src/com/javaGelisme/Main.java
@@ -3,6 +3,7 @@ package com.javaGelisme;
 public class Main {
 
     public static void main(String[] args) {
-
+        Team team = new Team();
+        team.option();
     }
 }
--- src/com/javaGelisme/Team.java
+++ src/com/javaGelisme/Team.java
@@ -2,17 +2,33 @@ package com.javaGelisme;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Team {
     private List<String> teams = new ArrayList<String>();
     public Team(){
         teams.add("Galatasaray");
         teams.add("Bursaspor");
-        teams.add("Fenerbahçe");
-        teams.add("Beşiktaş");
-        teams.add("Başakşehir");
+        teams.add("Fenerbahce");
+        teams.add("Besiktas");
+        teams.add("Basaksehir");
         teams.add("Trabzonspor");
-        
     }
 
+    public void option(){
+        List<String> newList = new ArrayList<String>();
+
+        for (int i=1; i <= 10; i++){
+
+        }
+    }
+
+    public static String getRandom(List<String> array){
+        int rnd = new Random().nextInt(array.size());
+        return array.get(rnd);
+    }
+
+    public static void selectFirstRandom(){
+
+    }
 }
