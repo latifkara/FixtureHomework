90bad553d42014204196d6863141d8d5bba7c34f
latifkara <latifkara93@gmail.com>
2022-12-19 16:35:38 +0300

    Initial commit

---
+++ src/com/javaGelisme/Main.java
@@ -0,0 +1,8 @@
+package com.javaGelisme;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/javaGelisme/Team.java
@@ -0,0 +1,18 @@
+package com.javaGelisme;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team {
+    private List<String> teams = new ArrayList<String>();
+    public Team(){
+        teams.add("Galatasaray");
+        teams.add("Bursaspor");
+        teams.add("Fenerbahçe");
+        teams.add("Beşiktaş");
+        teams.add("Başakşehir");
+        teams.add("Trabzonspor");
+        
+    }
+
+}
